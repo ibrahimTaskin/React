255dcced8cc33314f4c8405e7cfcb62fc7f57191
ibrahim taskin <taskin.ibrahim1@gmail.com>
2020-06-17 22:35:41 +0300

    Navigation category product file adding

---
+++ src/Navigate.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navigate extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
