73cfef0b8a725a955d0ae9331ab1191d1c7985fd
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2020-06-22 21:42:53 +0300

    Added new pages

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                This Page Not Found =)
+            </div>
+        )
+    }
+}
---
+++ src/productDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class productDetail extends Component {
+    render() {
+        return (
+            <div>
+                This page product detail !
+            </div>
+        )
+    }
+}
