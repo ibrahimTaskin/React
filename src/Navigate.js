740efaf9f94e0b9aff9a2e3e2e91402e9369cd33
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2020-06-23 20:47:16 +0300

    Submit Example

--- src/Navigate.js
+++ src/Navigate.js
@@ -1,51 +1,58 @@
 import React, { Component } from "react";
-import { Collapse,
+import {
+  Collapse,
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
   NavLink,
-  NavbarText } from "reactstrap";
-  import {Link} from 'react-router-dom'
+  NavbarText,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 export default class Navigate extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.toogle=this.toogle.bind(this);
-        this.state={
-            isOpen:false
-        };
-    }
-    toogle(){
-        this.setState({
-            isOpen:this.state.isOpen
-        });
-    }
+    this.toogle = this.toogle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toogle() {
+    this.setState({
+      isOpen: this.state.isOpen,
+    });
+  }
   render() {
     return (
       <div>
         <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <Link to="/" >Anasayfa</Link>
-            </NavItem>
-            <NavItem>
-            <Link to="form1" >Form</Link>
-            </NavItem>          
-            <NavItem>
-            <Link to="product" >Ürün Detay</Link>
-            </NavItem>     
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
-      </Navbar>
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink>
+                  <Link to="/">Anasayfa</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="form1">Form</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="product">Ürün Detay</Link>
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
+        </Navbar>
         <hr />
-        
       </div>
     );
   }
--- src/formExams.js
+++ src/formExams.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import alertify from "alertifyjs";
 
 export default class formExams extends Component {
-    state={
-        username:''
-    }
+  state = {
+    username: "",
+  };
 
-    onChangeHandler=(event)=>{
-this.setState({username:event.target.value});
-    }
-    render() {
-        return (
-            <div>
-                <form>
-                    <input onChange={this.onChangeHandler} type="text"></input>
-                </form>
-        <h3> {this.state.username}</h3>
-            </div>
-        )
-    }
+  onChangeHandler = (event) => {
+    this.setState({ username: event.target.value });
+  };
+
+  onSubmitHandler = (event) => {
+    event.preventDefault();
+    //alert(this.state.username);
+    alertify.success("Sen kimsim ? " + this.state.username,2);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmitHandler}>
+          <input onChange={this.onChangeHandler} type="text"></input>
+          <h3> {this.state.username}</h3>
+          <input type="submit" value="Kaydet"></input>
+        </form>
+      </div>
+    );
+  }
 }
